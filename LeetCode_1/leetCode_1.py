90c22d1ceb6e19eddb76289204a83129af1f9ba2
adriankwan108 <adriankwan108@gmail.com>
2020-06-19 22:45:25 +0800

    Update leetCode_1.py

--- LeetCode_1/leetCode_1.py
+++ LeetCode_1/leetCode_1.py
@@ -5,3 +5,16 @@ class Solution:
             if( target - num in temp):
                 return temp[target-num],  i
             temp[num]=i
+
+'''
+if __name__ == '__main__':
+    sol = Solution()
+    testCase = [3,3,4,1]
+    answer = sol.twoSum(testCase, 5)
+    print(answer)
+'''
+
+'''
+Runtime: 48 ms, faster than 80.93% of Python3 online submissions for Two Sum.
+Memory Usage: 15.4 MB, less than 19.99% of Python3 online submissions for Two Sum.
+'''
