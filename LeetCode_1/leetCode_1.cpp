0a5d8419c5a84e9aa42fc0be64aeff9c2c2a7ac3
adriankwan108 <adriankwan108@gmail.com>
2021-09-09 20:23:18 +0800

    Create leetCode_1.cpp

---
+++ LeetCode_1/leetCode_1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        //std::unordered_map is better than std::map
+        std::unordered_map<int,int> mapping;
+        int complement;
+
+        for(int i = 0; i< nums.size(); i++)
+        {
+            complement = target-nums[i];
+
+            //find if the complement is inside our visited map
+            if(mapping.count(complement))   //if true, found and return
+            {
+                return {mapping[complement],i};
+            }
+
+            //if cannot find, add the current number in vector<int> nums to the mapping
+            mapping.insert(std::pair<int,int>(nums[i],i));
+        }
+
+        //if nothing, return nothing
+        return {};
+    }
+};
