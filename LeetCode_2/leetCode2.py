dd440beb914f16603ecf0b52ab3896f35c20581b
adriankwan108 <adriankwan108@gmail.com>
2021-09-10 19:54:28 +0800

    Create leetCode2.py

---
+++ LeetCode_2/leetCode2.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        carry = 0
+        current = ListNode(0)
+        result = current
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            carry, val = divmod(carry, 10)
+            current.next = ListNode(val)
+            current = current.next
+            
+        return result.next
+
+#68~103ms, 13.3MB
