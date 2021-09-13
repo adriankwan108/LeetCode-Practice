1faebcd138243f2e2092688e634c3c858feee0c3
adriankwan108 <adriankwan108@gmail.com>
2021-09-13 19:11:59 +0800

    Update SameTree_100.cpp
    comments minor mistakes

--- BinarySearchTreeQuestion/Easy/SameTree_100.cpp
+++ BinarySearchTreeQuestion/Easy/SameTree_100.cpp
@@ -4,7 +4,7 @@ public:
         //this means checking if there exists nullptr in both two nodes
         //if one of them is null, check if both of them are nullptrs  (if(!node1||!node2))
         //if both are nullptrs, return true, if not, return false (return (node1==node2))
-        //if none of them are null, continue to line 13
+        //if none of them are null, continue to line 10
         if(!p||!q) return (p==q);
         
         if(p->val != q->val) return false;
