518def1d13b7a413f86d66a3c981284f718e945b
adriankwan108 <adriankwan108@gmail.com>
2021-09-13 19:09:53 +0800

    cleaner coding
    cleaner 101.cpp by logics, lesser "if" sections
    while transit the explanation of simpler coding to 100.cpp

--- BinarySearchTreeQuestion/Easy/SameTree_100.cpp
+++ BinarySearchTreeQuestion/Easy/SameTree_100.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:    
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==nullptr && q==nullptr) return true;
-        if(p==nullptr || q==nullptr) return false;
+        //this means checking if there exists nullptr in both two nodes
+        //if one of them is null, check if both of them are nullptrs  (if(!node1||!node2))
+        //if both are nullptrs, return true, if not, return false (return (node1==node2))
+        //if none of them are null, continue to line 13
+        if(!p||!q) return (p==q);
+        
         if(p->val != q->val) return false;
         
-        if( isSameTree(p->left, q->left) == true ){
-            if(isSameTree(p->right, q->right) == true)
-            {
-                return true;
-            }else{
-                return false;
-            }
-        }else{
-            return false;
-        }
-
-        return false;
+        return isSameTree(p->left, q->left)&&isSameTree(p->right, q->right);
     }
 };
-//0~4ms, 10MB
+//0~4ms, 10.1MB
--- BinarySearchTreeQuestion/Easy/SymmetricTree_101.cpp
+++ BinarySearchTreeQuestion/Easy/SymmetricTree_101.cpp
@@ -3,10 +3,6 @@ public:
     stack<int> stack;
     bool compare(TreeNode* node1, TreeNode* node2)
     {
-        //this means checking if there exists nullptr in both two nodes
-        //if one of them is null, check if both of them are nullptrs  (if(!node1||!node2))
-        //if both are nullptrs, return true, if not, return false (return (node1==node2))
-        //if none of them are null, continue to line 13
         if(!node1||!node2) return (node1==node2);
         
         if(node1->val != node2->val) return false;
