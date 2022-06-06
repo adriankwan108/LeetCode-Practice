7e9169daf3ef1f0728a0c7543ad0c1b837457ba8
adriankwan108 <adriankwan108@gmail.com>
2022-06-06 19:01:20 +0800

    Update 1_TwoSum.cpp

--- Easy/1_TwoSum.cpp
+++ Easy/1_TwoSum.cpp
@@ -2,25 +2,23 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) 
     {
-        //std::unordered_map is better than std::map
-        std::unordered_map<int,int> mapping;
-        int complement;
-
-        for(int i = 0; i< nums.size(); i++)
+        /*
+            algo: 
+            1. iterate nums and put <num, index> into hashMap
+            2. for each iteration, find (target - data) in the hashMap
+            3. if not, iterate next
+        */
+        
+        unordered_map<int,int> hashMap; //<number, index>
+        vector<int> result;
+        for(int i=0; i<nums.size(); i++)
         {
-            complement = target-nums[i];
-
-            //find if the complement is inside our visited map
-            if(mapping.count(complement))   //if true, found and return
-            {
-                return {mapping[complement],i};
-            }
-
-            //if cannot find, add the current number in vector<int> nums to the mapping
-            mapping.insert(std::pair<int,int>(nums[i],i));
+            int complement = target-nums[i];
+            if(hashMap.count(complement)==1)
+                return {hashMap[complement],i};
+            else
+                hashMap[nums[i]] = i;
         }
-
-        //if nothing, return nothing
         return {};
     }
 };
